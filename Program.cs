2b580b5cd6164fa72f5af9eeeaa0bbb2f62ff90c
DanielDarwiche <danielinti1@hotmail.com>
2023-06-06 17:21:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Xunit_w_AcceptanceTests_TDD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ UnitTests_w_XUnit/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace UnitTests_w_XUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
